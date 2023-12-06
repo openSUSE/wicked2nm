b58cce1facad430465b8f5b5f5f6c841bed2e58d
Clemens Famulla-Conrad <cfamullaconrad@suse.de>
2023-12-06 17:25:48 +0100

    Revert "Merge pull request #14 from cfconrad/pr_fix_bond_options"
    This reverts commit 041fbabac418ceace997fd6e8584e261dfdb082b, reversing
    changes made to 60766d537e35838b5eace9294bc56417443b555f.
    
    Revert "Merge pull request #10 from cfconrad/pr_finalize_bond_options"
    
    This reverts commit fe9667381999e28cec18f7a6ce347634e23bed5c, reversing
    changes made to cf1a7b1b0927113b207ea60ed330aadd46069e39.
    
    Revert "Merge pull request #1 from cfconrad/impl_bonding"
    
    This reverts commit dabe0c09b1c4a6a2a6a39131dae3a84cc9879f95, reversing
    changes made to 518d236473b9a7bdcb70e65197440495de83289c.

--- src/interface.rs
+++ src/interface.rs
@@ -1,11 +1,11 @@
-use agama_dbus_server::network::model::{self, IpConfig, IpRoute, Ipv4Method, Ipv6Method, Parent};
+use agama_dbus_server::network::model::{self, IpConfig, IpRoute, Ipv4Method, Ipv6Method};
 use cidr::IpInet;
-use serde::{Deserialize, Deserializer, Serialize};
+use serde::{Deserialize, Serialize};
 use serde_with::{
     formats::CommaSeparator, serde_as, skip_serializing_none, DeserializeFromStr, SerializeDisplay,
     StringWithSeparator,
 };
-use std::{collections::HashMap, net::IpAddr, str::FromStr};
+use std::{net::IpAddr, str::FromStr};
 use strum_macros::{Display, EnumString};
 
 #[derive(Debug, PartialEq, Default, Serialize, Deserialize)]
@@ -14,7 +14,6 @@ pub struct Interface {
     pub name: String,
     pub control: Control,
     pub firewall: Firewall,
-    pub link: Link,
     pub ipv4: Ipv4,
     #[serde(rename = "ipv4-static", skip_serializing_if = "Option::is_none")]
     pub ipv4_static: Option<Ipv4Static>,
@@ -25,15 +24,8 @@ pub struct Interface {
     pub ipv6_dhcp: Option<Ipv6Dhcp>,
     #[serde(rename = "ipv6-auto", skip_serializing_if = "Option::is_none")]
     pub ipv6_auto: Option<Ipv6Auto>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub bond: Option<Bond>,
     #[serde(rename = "@origin")]
     pub origin: String,
-}
-
-#[derive(Debug, PartialEq, Serialize, Clone, Deserialize)]
-pub enum ParentKind {
-    Bond,
 }
 
 #[derive(Debug, PartialEq, Default, Serialize, Deserialize)]
@@ -45,15 +37,6 @@ pub struct Control {
 #[derive(Debug, PartialEq, Default, Serialize, Deserialize)]
 #[serde(default)]
 pub struct Firewall {}
-
-#[derive(Debug, PartialEq, Default, Serialize, Deserialize)]
-#[serde(default)]
-pub struct Link {
-    #[serde(rename = "master", skip_serializing_if = "Option::is_none")]
-    pub parent: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub kind: Option<ParentKind>,
-}
 
 #[derive(Debug, PartialEq, Default, Serialize, Deserialize)]
 #[serde(default)]
@@ -149,298 +132,6 @@ pub struct Nexthop {
     pub gateway: String,
 }
 
-#[derive(Debug, PartialEq, SerializeDisplay, DeserializeFromStr, EnumString, Display)]
-#[strum(serialize_all = "kebab_case")]
-pub enum XmitHashPolicy {
-    Layer2,
-    Layer23,
-    Layer34,
-    Encap23,
-    Encap34,
-}
-
-#[derive(Debug, PartialEq, SerializeDisplay, DeserializeFromStr, EnumString, Display)]
-#[strum(serialize_all = "kebab_case")]
-pub enum LacpRate {
-    Slow,
-    Fast,
-}
-
-#[derive(Debug, PartialEq, SerializeDisplay, DeserializeFromStr, EnumString, Display)]
-#[strum(serialize_all = "kebab_case")]
-pub enum AdSelect {
-    Stable,
-    Bandwidth,
-    Count,
-}
-
-#[derive(Debug, PartialEq, SerializeDisplay, DeserializeFromStr, EnumString, Display)]
-#[strum(serialize_all = "kebab_case")]
-pub enum PrimaryReselect {
-    Always,
-    Better,
-    Failure,
-}
-
-#[derive(Debug, PartialEq, SerializeDisplay, DeserializeFromStr, EnumString, Display)]
-#[strum(serialize_all = "kebab_case")]
-pub enum BondMode {
-    BalanceRr = 0,
-    ActiveBackup,
-    BalanceXor,
-    Broadcast,
-    #[strum(serialize = "802.3ad")]
-    IEEE8023ad,
-    BalanceTlb,
-    BalanceAlb,
-}
-
-#[derive(Debug, PartialEq, Serialize, Deserialize)]
-#[skip_serializing_none]
-#[serde(rename_all = "kebab-case")]
-pub struct Bond {
-    pub mode: BondMode,
-    pub miimon: Option<Miimon>,
-    pub arpmon: Option<ArpMon>,
-    #[serde(deserialize_with = "unwrap_slaves")]
-    pub slaves: Vec<Slave>,
-    /* only on mode=[802.3ad, balance_xor] */
-    pub xmit_hash_policy: Option<XmitHashPolicy>,
-    /* only on mode=balance_rr */
-    pub packets_per_slave: Option<u32>,
-    /* only on mode=balance_tlb */
-    pub tlb_dynamic_lb: Option<bool>,
-    /* only on mode=802.3ad */
-    pub lacp_rate: Option<LacpRate>,
-    /* only on mode=802.3ad */
-    pub ad_select: Option<AdSelect>,
-    /* only on mode=802.3ad */
-    pub ad_user_port_key: Option<u32>,
-    /* only on mode=802.3ad */
-    pub ad_actor_sys_prio: Option<u32>,
-    /* only on mode=802.3ad */
-    pub ad_actor_system: Option<String>,
-    /* only on mode=802.3ad */
-    pub min_links: Option<u32>,
-    /* only on mode=active-backup */
-    pub primary_reselect: Option<PrimaryReselect>,
-    /* only on mode=active-backup */
-    pub fail_over_mac: Option<FailOverMac>,
-    /* only on mode=active-backup */
-    pub num_grat_arp: Option<u32>,
-    /* only on mode=active-backup */
-    pub num_unsol_na: Option<u32>,
-    /* only on mode=[balance_tlb|balance_alb] */
-    pub lp_interval: Option<u32>,
-    /* only on mode=[balance_tlb|balance_alb|balance_RR|active-backup] */
-    pub resend_igmp: Option<u32>,
-    pub all_slaves_active: Option<bool>,
-    pub address: Option<String>,
-}
-
-impl Bond {
-    pub fn primary(self: &Bond) -> Option<&String> {
-        for s in self.slaves.iter() {
-            if s.primary.is_some() && s.primary.unwrap() {
-                return Some(&s.device);
-            }
-        }
-        None
-    }
-}
-
-#[derive(Debug, PartialEq, Default, Serialize, Deserialize)]
-pub struct Slave {
-    pub device: String,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub primary: Option<bool>,
-}
-
-#[derive(Debug, PartialEq, Default, SerializeDisplay, DeserializeFromStr, EnumString, Display)]
-#[strum(serialize_all = "kebab_case")]
-pub enum CarrierDetect {
-    Ioctl = 0,
-    #[default]
-    Netif = 1,
-}
-
-#[derive(Debug, PartialEq, Default, Serialize, Deserialize)]
-pub struct Miimon {
-    pub frequency: u32,
-    #[serde(rename = "carrier-detect")]
-    pub carrier_detect: CarrierDetect,
-    pub downdelay: Option<u32>,
-    pub updelay: Option<u32>,
-}
-
-#[derive(Debug, PartialEq, SerializeDisplay, DeserializeFromStr, EnumString, Display)]
-#[strum(serialize_all = "kebab_case")]
-pub enum ArpValidateTargets {
-    Any = 0,
-    All = 1,
-}
-
-#[derive(Debug, PartialEq, SerializeDisplay, DeserializeFromStr, EnumString, Display)]
-#[strum(serialize_all = "snake_case")]
-pub enum ArpValidate {
-    None = 0,
-    Active = 1,
-    Backup = 2,
-    All = 3,
-    Filter = 4,
-    FilterActive = 5,
-    FilterBackup = 6,
-}
-
-#[derive(Debug, PartialEq, Serialize, Deserialize)]
-pub struct ArpMon {
-    pub interval: u32,
-    pub validate: ArpValidate,
-    #[serde(rename = "validate-targets", skip_serializing_if = "Option::is_none")]
-    pub validate_targets: Option<ArpValidateTargets>,
-    #[serde(deserialize_with = "unwrap_arpmon_targets")]
-    pub targets: Vec<String>,
-}
-
-fn unwrap_arpmon_targets<'de, D>(deserializer: D) -> Result<Vec<String>, D::Error>
-where
-    D: Deserializer<'de>,
-{
-    #[derive(Debug, PartialEq, Default, Serialize, Deserialize)]
-    pub struct ArpMonTargetAddressV4 {
-        #[serde(rename = "ipv4-address")]
-        pub ipv4_address: Vec<String>,
-    }
-    Ok(ArpMonTargetAddressV4::deserialize(deserializer)?.ipv4_address)
-}
-
-fn unwrap_slaves<'de, D>(deserializer: D) -> Result<Vec<Slave>, D::Error>
-where
-    D: Deserializer<'de>,
-{
-    #[derive(Debug, PartialEq, Default, Serialize, Deserialize)]
-    struct Slaves {
-        slave: Vec<Slave>,
-    }
-    Ok(Slaves::deserialize(deserializer)?.slave)
-}
-
-impl From<&Bond> for model::BondConfig {
-    fn from(bond: &Bond) -> model::BondConfig {
-        let mut h: HashMap<String, String> = HashMap::new();
-
-        h.insert(String::from("mode"), bond.mode.to_string());
-        if let Some(p) = bond.primary() {
-            h.insert(String::from("primary"), p.clone());
-        }
-
-        if let Some(m) = &bond.miimon {
-            h.insert(String::from("miimon"), format!("{}", m.frequency));
-            h.insert(
-                String::from("use_carrier"),
-                match m.carrier_detect {
-                    CarrierDetect::Ioctl => 0,
-                    CarrierDetect::Netif => 1,
-                }
-                .to_string(),
-            );
-            if let Some(v) = m.downdelay {
-                h.insert(String::from("downdelay"), v.to_string());
-            }
-            if let Some(v) = m.updelay {
-                h.insert(String::from("updelay"), v.to_string());
-            }
-        }
-
-        if let Some(a) = &bond.arpmon {
-            h.insert(String::from("arp_interval"), format!("{}", a.interval));
-            h.insert(String::from("arp_validate"), a.validate.to_string());
-
-            if !a.targets.is_empty() {
-                let sv = a
-                    .targets
-                    .iter()
-                    .map(|c| c.as_ref())
-                    .collect::<Vec<&str>>()
-                    .join(",");
-                h.insert(String::from("arp_ip_target"), sv);
-            }
-
-            if let Some(v) = &a.validate_targets {
-                h.insert(String::from("arp_all_targets"), v.to_string());
-            }
-        }
-
-        if let Some(fom) = &bond.fail_over_mac {
-            h.insert(String::from("fail_over_mac"), fom.to_string());
-        }
-
-        if let Some(v) = &bond.xmit_hash_policy {
-            h.insert(String::from("xmit_hash_policy"), v.to_string());
-        }
-
-        if let Some(v) = &bond.packets_per_slave {
-            h.insert(String::from("packets_per_slave"), v.to_string());
-        }
-
-        if let Some(v) = &bond.tlb_dynamic_lb {
-            h.insert(
-                String::from("tlb_dynamic_lb"),
-                if *v { 1.to_string() } else { 0.to_string() },
-            );
-        }
-
-        if let Some(v) = &bond.lacp_rate {
-            h.insert(String::from("lacp_rate"), v.to_string());
-        }
-
-        if let Some(v) = &bond.ad_select {
-            h.insert(String::from("ad_select"), v.to_string());
-        }
-        if let Some(v) = &bond.ad_user_port_key {
-            h.insert(String::from("ad_user_port_key"), v.to_string());
-        }
-        if let Some(v) = &bond.ad_actor_sys_prio {
-            h.insert(String::from("ad_actor_sys_prio"), v.to_string());
-        }
-        if let Some(v) = &bond.ad_actor_system {
-            h.insert(String::from("ad_actor_system"), v.clone());
-        }
-        if let Some(v) = &bond.min_links {
-            h.insert(String::from("min_links"), v.to_string());
-        }
-        if let Some(v) = &bond.primary_reselect {
-            h.insert(String::from("primary_reselect"), v.to_string());
-        }
-        if let Some(v) = &bond.num_grat_arp {
-            h.insert(String::from("num_grat_arp"), v.to_string());
-        }
-        if let Some(v) = &bond.num_unsol_na {
-            h.insert(String::from("num_unsol_na"), v.to_string());
-        }
-        if let Some(v) = &bond.lp_interval {
-            h.insert(String::from("lp_interval"), v.to_string());
-        }
-        if let Some(v) = &bond.resend_igmp {
-            h.insert(String::from("resend_igmp"), v.to_string());
-        }
-        if let Some(v) = &bond.all_slaves_active {
-            h.insert(
-                String::from("all_slaves_active"),
-                if *v { 1.to_string() } else { 0.to_string() },
-            );
-        }
-
-        model::BondConfig {
-            options: h,
-            hwaddr: match &bond.address {
-                Some(s) => model::MacAddr::try_from(s.as_ref()).ok(),
-                _ => None,
-            },
-        }
-    }
-}
-
 pub struct ConnectionResult {
     pub connection: model::Connection,
     pub warnings: Vec<anyhow::Error>,
@@ -454,33 +145,15 @@ pub struct IpConfigResult {
 impl Interface {
     pub fn to_connection(&self) -> Result<ConnectionResult, anyhow::Error> {
         let ip_config = self.to_ip_config()?;
-        let mut base = model::BaseConnection {
+        let base = model::BaseConnection {
             id: self.name.clone(),
             interface: self.name.clone(),
             ip_config: ip_config.ip_config,
             ..Default::default()
         };
 
-        if self.link.kind.is_some() && self.link.parent.is_some() {
-            let interface = self.link.parent.clone().unwrap();
-            let kind = match &self.link.kind {
-                Some(p) => match &p {
-                    ParentKind::Bond => model::ParentKind::Bond,
-                },
-                None => return Err(anyhow::anyhow!("Missing ParentType for {}", self.name)),
-            };
-            base.parent = Some(Parent { interface, kind });
-        }
-
         Ok(ConnectionResult {
-            connection: if let Some(b) = &self.bond {
-                model::Connection::Bond(model::BondConnection {
-                    base,
-                    bond: b.into(),
-                })
-            } else {
-                model::Connection::Ethernet(model::EthernetConnection { base })
-            },
+            connection: model::Connection::Ethernet(model::EthernetConnection { base }),
             warnings: ip_config.warnings,
         })
     }
@@ -743,93 +416,5 @@ mod tests {
         assert_eq!(static_connection.base().ip_config.method4, Ipv4Method::Auto);
         assert_eq!(static_connection.base().ip_config.method6, Ipv6Method::Auto);
         assert_eq!(static_connection.base().ip_config.addresses.len(), 0);
-    }
-
-    #[test]
-    fn test_bond_options() {
-        let bond = Bond {
-            mode: BondMode::IEEE8023ad,
-            xmit_hash_policy: Some(XmitHashPolicy::Encap34),
-            fail_over_mac: Some(FailOverMac::Active),
-            packets_per_slave: Some(23),
-            tlb_dynamic_lb: Some(true),
-            lacp_rate: Some(LacpRate::Slow),
-            ad_select: Some(AdSelect::Bandwidth),
-            ad_user_port_key: Some(42),
-            ad_actor_sys_prio: Some(5),
-            ad_actor_system: Some(String::from("00:de:ad:be:ef:00")),
-            min_links: Some(3),
-            primary_reselect: Some(PrimaryReselect::Better),
-            num_grat_arp: Some(7),
-            num_unsol_na: Some(13),
-            lp_interval: Some(17),
-            resend_igmp: Some(19),
-            all_slaves_active: Some(true),
-            miimon: Some(Miimon {
-                frequency: 42,
-                carrier_detect: CarrierDetect::Netif,
-                downdelay: Some(23),
-                updelay: Some(5),
-            }),
-            arpmon: Some(ArpMon {
-                interval: 23,
-                validate: ArpValidate::FilterBackup,
-                validate_targets: Some(ArpValidateTargets::Any),
-                targets: vec![String::from("1.2.3.4"), String::from("4.3.2.1")],
-            }),
-            slaves: vec![],
-            address: Some(String::from("02:11:22:33:44:55")),
-        };
-
-        let bondconfig: model::BondConfig = (&bond).into();
-        let s = HashMap::from([
-            ("xmit_hash_policy", String::from("encap34")),
-            ("packets_per_slave", 23.to_string()),
-            ("tlb_dynamic_lb", 1.to_string()),
-            ("lacp_rate", String::from("slow")),
-            ("ad_select", String::from("bandwidth")),
-            ("ad_user_port_key", 42.to_string()),
-            ("ad_actor_sys_prio", 5.to_string()),
-            ("ad_actor_system", String::from("00:de:ad:be:ef:00")),
-            ("min_links", 3.to_string()),
-            ("primary_reselect", String::from("better")),
-            ("fail_over_mac", String::from("active")),
-            ("num_grat_arp", 7.to_string()),
-            ("num_unsol_na", 13.to_string()),
-            ("lp_interval", 17.to_string()),
-            ("resend_igmp", 19.to_string()),
-            ("all_slaves_active", 1.to_string()),
-            // miimon
-            ("miimon", 42.to_string()),
-            ("use_carrier", 1.to_string()),
-            ("downdelay", 23.to_string()),
-            ("updelay", 5.to_string()),
-            // arpmon
-            ("arp_validate", String::from("filter_backup")),
-            ("arp_all_targets", String::from("any")),
-            ("arp_ip_target", String::from("1.2.3.4,4.3.2.1")),
-            ("arp_interval", 23.to_string()),
-        ]);
-
-        for (k, v) in s.iter() {
-            assert!(
-                bondconfig.options.contains_key(*k),
-                "Missing key '{}' in bond hash {:?}",
-                *k,
-                bondconfig
-            );
-            assert_eq!(
-                bondconfig.options.get(*k).unwrap(),
-                v,
-                "Unexpected value '{}' in key '{}'",
-                *k,
-                v
-            );
-        }
-
-        assert_eq!(
-            bondconfig.hwaddr.unwrap().data,
-            [0x02, 0x11, 0x22, 0x33, 0x44, 0x55]
-        );
     }
 }
